7bf267b8807b1b107308bf26420c67409f31ea9a
pR0Ps <pR0Ps.cm@gmail.com>
2014-02-10 21:17:19 -0500

    Add login button/indicator for Android and iOS
    Leaderboards menu item is not displayed on platforms that don't
    have leaderboards.
    
    Social button does a cool shake when trying to access leaderboards
    while offline.

--- Assets/Scripts/Buttons/LeaderboardsButton.cs
+++ Assets/Scripts/Buttons/LeaderboardsButton.cs
@@ -2,17 +2,26 @@
 using System.Collections;
 
 public class LeaderboardsButton : InteractiveObject {
+
+	public SocialButton socialButton;
+
+	public void Awake(){
+		#if (!UNITY_ANDROID && !UNITY_IPHONE)
+			gameObject.SetActive(false);
+		#endif
+	}
 	
 	public override void TapAction () {
 		if (Social.localUser.authenticated){
 			#if UNITY_ANDROID
 			((GooglePlayGames.PlayGamesPlatform)Social.Active).ShowLeaderboardUI("30-move");
-			#else
+			#elif UNITY_IPHONE
 			Social.ShowLeaderboardUI();
 			#endif
 		}
 		else{
 			Debug.Log("Not logged in, can't show leaderboards");
+			if (socialButton != null) socialButton.Shake();
 		}
 	}
 }

c8160308c9b0c7abcf155ae4d38e58d543d82b60
pR0Ps <pR0Ps.cm@gmail.com>
2014-03-13 21:35:52 -0400

    Added some utility functions

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Utils {
+
+	//From http://stackoverflow.com/a/8001594
+	public static T[] ConcatArrays<T>(params T[][] list){
+		var result = new T[list.Sum(a => a.Length)];
+		int offset = 0;
+		for (int x = 0; x < list.Length; x++){
+			list[x].CopyTo(result, offset);
+			offset += list[x].Length;
+		}
+		return result;
+	}
+
+	//Take the arrays in the enumerator and concatenate them into a single array
+	public static T[] Unpack<T>(IEnumerable<T[]> gen){
+		return ConcatArrays<T> (gen.ToArray());
+	}
+}

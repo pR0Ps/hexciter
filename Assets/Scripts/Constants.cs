4a482df0b3104ed3c8da24a9e94f1c59d0d1ff52
Broxxar <danjohnmoran@gmail.com>
2014-03-21 14:04:20 -0400

    Color Palette

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,30 +1,38 @@
 using UnityEngine;
 using System.Collections;
+using System;
 using System.Collections.Generic;
 
 public static class Constants {
 	
-	public const int NUMBER_OF_COLORS = 5;
-	
+	public const int NUMBER_OF_COLORS = 4;
+
 	public enum HexColors {
 		Blue,
 		//Orange,
-		Purple,
+		//Purple,
 		Green,
 		Yellow,
 		Red,
 		Grey
 	}
 	
-	public static Color[] Colors = new Color[] {
-		new Color (0.206f, 0.728f, 0.801f, 1.0f),
-		//new Color (0.801f, 0.523f, 0.171f, 1.0f),
-		new Color (0.462f, 0.203f, 0.691f, 1.0f),
-		new Color (0.175f, 0.699f, 0.222f, 1.0f),
-		new Color (0.868f, 0.772f, 0.236f, 1.0f),
-		new Color (0.728f, 0.177f, 0.177f, 1.0f),
-		new Color (0.500f, 0.500f, 0.500f, 1.0f),
+	public static Color32[] Colors = new Color32[] {
+		HexToColor32("E40045"),
+		HexToColor32("FF7600"),
+		HexToColor32("01939A"),
+		HexToColor32("67E300"),
+		new Color32 (128, 128, 128, 255)
 	};
+
+	static Color32 HexToColor32 (string hexadecimal) {
+		
+		byte r = Convert.ToByte (hexadecimal.Substring (0, 2), 16);
+		byte g = Convert.ToByte (hexadecimal.Substring (2, 2), 16);
+		byte b = Convert.ToByte (hexadecimal.Substring (4, 2), 16);
+
+		return new Color32 (r, g, b, 255);
+	}
 
 	public static float CUBE_LOOK_DIST = 150.0f; // the closer this is to 0, the more intense the look effect
 	public static float CUBE_LOOK_SPEED = 10.0f;

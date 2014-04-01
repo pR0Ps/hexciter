11fa7685a9dcd120b02fbd269576cde369301f57
Broxxar <danjohnmoran@gmail.com>
2014-03-31 23:08:35 -0400

    Add screenshots, poster, changed menu particles

---
+++ Assets/Scripts/ScreenShotTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShotTool : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Application.CaptureScreenshot("screenshot"+ System.DateTime.Now.ToString("yyyyMMddHHmmssfff") +".png", 2);
+		}
+	}
+}

9024e30503278f19e40989980b8a5cd1e49a4617
Broxxar <danjohnmoran@gmail.com>
2014-04-02 21:59:31 -0400

    Tutorial
    Added a guided tutorial to teach players the basics of the game. Not
    linked from the menu yet.

--- Assets/Scripts/GridLogic.cs
+++ Assets/Scripts/GridLogic.cs
@@ -23,6 +23,10 @@ public class GridLogic : MonoBehaviour {
 	public GameoverDialog gameoverDialog;
 	public bool disabled { get; private set; }
 
+	bool tutorialEnabled = false;
+	bool flooded = false;
+	bool destroyed = false;
+
 	void Awake () {
 		// tell the game to run at 60 fps, maybe put this some where better later
 		Application.targetFrameRate = 60;
@@ -32,11 +36,9 @@ public class GridLogic : MonoBehaviour {
 	}
 
 	void Start () {
-		level = 1;
-		score = 0;
-		startScore = 0;
-		targetScore = 5000;
-		colorSelector.Init ();
+		disabled = true;
+		if (Application.loadedLevelName == "tutorial")
+			tutorialEnabled = true;
 		origin = transform.FindChild("Origin").GetComponent<GridPlace>();
 		
 		for (int i = 1; i < 61; i++) {
@@ -44,11 +46,19 @@ public class GridLogic : MonoBehaviour {
 		}
 		gridPlaces[60] = origin;
 
-		FadeCam.Instance.FadeIn(() => {StartCoroutine(StartGame());});
+		if (!tutorialEnabled)
+			FadeCam.Instance.FadeIn(() => {StartCoroutine(StartGame());});
+		else
+			FadeCam.Instance.FadeIn(() => {StartCoroutine(Tutorial());});
 	}
 
-	IEnumerator StartGame(){
-		disabled = true;
+	IEnumerator StartGame() {
+		level = 1;
+		score = 0;
+		startScore = 0;
+		targetScore = 5000;
+		colorSelector.Init ();
+		
 		StartCoroutine(Utils.SlowSpawnSiblings(origin));
 		while (GridBusy()) {
 			yield return new WaitForEndOfFrame();
@@ -131,6 +141,7 @@ public class GridLogic : MonoBehaviour {
 	public void Flood(GridPlace start) {
 		lastMoveScore /= 4; // halves your combo bonus
 		if (!start.busy){
+			flooded = true;
 			StartCoroutine(Utils.FillSiblings(start, colorSelector.Current()));
 			AudioController.Instance.PlaySound ("fillchime");
 			DoMove();
@@ -169,6 +180,7 @@ public class GridLogic : MonoBehaviour {
 	}
 
 	public void Destroy (GridPlace start) {
+		destroyed = true;
 		StartCoroutine(Utils.KillSiblings(start, KillCallback(start, colorSelector.Current())));
 		AudioController.Instance.PlaySound ("chime1");
 		DoMove();
@@ -178,9 +190,12 @@ public class GridLogic : MonoBehaviour {
 	public void DoMove(){
 		//Update count and color chooser
 		moves.DoMove();
-		colorSelector.NewColor();
+		if (!tutorialEnabled)
+			colorSelector.NewColor();
+		else
+			colorSelector.NewColor(0);
 	}
-
+	
 	void GameOver () {
 
 		int best = PlayerPrefs.GetInt ("Best_Score");
@@ -196,8 +211,198 @@ public class GridLogic : MonoBehaviour {
 	}
 
 	void Update () {
-		if (!disabled && (score >= targetScore || moves.NoneLeft())){
+		if (!disabled && !tutorialEnabled &&(score >= targetScore || moves.NoneLeft())){
 			StartCoroutine (NextLevel());
 		}
 	}
+
+	IEnumerator Tutorial() {
+		
+		TutorialText tText = GameObject.Find ("TutorialText").GetComponentInChildren<TutorialText> ();
+
+		gridPlaces[60].hexaCube.SlowSpawn(Constants.ChooseColor(2)); 
+		gridPlaces[59].hexaCube.SlowSpawn(Constants.ChooseColor(2)); 
+		gridPlaces[58].hexaCube.SlowSpawn(Constants.ChooseColor(0)); 
+		gridPlaces[57].hexaCube.SlowSpawn(Constants.ChooseColor(2)); 
+		gridPlaces[56].hexaCube.SlowSpawn(Constants.ChooseColor(0)); 
+		gridPlaces[55].hexaCube.SlowSpawn(Constants.ChooseColor(0)); 
+		gridPlaces[54].hexaCube.SlowSpawn(Constants.ChooseColor(0)); 
+
+		colorSelector.Init (0, 3);
+		colorSelector.DisableSwap ();
+
+		startScore = 0;
+		targetScore = 4000;
+
+		bool passed = false;
+
+		yield return StartCoroutine(ShowWaitTap (tText, "welcome to hexciter", "(tap to continue)"));
+
+		while (!passed) {
+			yield return StartCoroutine(tText.Show ("press and hold a hex to select", "select the center hex"));
+			yield return StartCoroutine(WaitForCenterSelect ());
+			yield return StartCoroutine(tText.Hide ());
+			colorSelector.GetComponentInChildren<TutorialArrow> ().FadeIn ();
+			yield return StartCoroutine(tText.Show ("swipe left to flood", "this uses the color shown here"));
+			yield return StartCoroutine(WaitForMoveCenter ());
+			colorSelector.GetComponentInChildren<TutorialArrow> ().FadeOut ();
+			disabled = true;
+
+			if (!flooded && !destroyed) {
+				yield return StartCoroutine(tText.Hide ());
+				continue;
+			}
+
+			if (flooded) {
+				passed = true;
+				break;
+			}
+
+			if (destroyed) {
+				break;
+			}
+		}
+		if (!passed) {
+			StartCoroutine(FailInstructions());
+			yield break;
+		}
+		yield return StartCoroutine(tText.Hide ());
+		yield return StartCoroutine(ShowWaitTap (tText, "well done!", "the hexes changed color"));
+		colorSelector.GetComponentInChildren<TutorialArrow> ().FadeIn ();
+		yield return StartCoroutine(ShowWaitTap (tText, "you'll always flood using this color", "(tap to continue)"));
+		yield return StartCoroutine(tText.Show ("tap the palette to swap colors", ""));
+		colorSelector.EnableSwap ();
+		yield return StartCoroutine (WaitForSwap ());
+		colorSelector.DisableSwap ();
+		yield return StartCoroutine(tText.Hide ());
+		colorSelector.GetComponentInChildren<TutorialArrow> ().FadeOut ();
+
+		passed = false;
+		flooded = false;
+		destroyed = false;
+
+		while (!passed) {
+			yield return StartCoroutine(tText.Show ("select the center hex", ""));
+			yield return StartCoroutine(WaitForCenterSelect ());
+			yield return StartCoroutine(tText.Hide ());
+			yield return StartCoroutine(tText.Show ("swipe right to destroy!", ""));
+			yield return StartCoroutine(WaitForMoveCenter ());
+			disabled = true;
+			
+			if (!flooded && !destroyed) {
+				yield return StartCoroutine(tText.Hide ());
+				continue;
+			}
+			
+			if (destroyed) {
+				passed = true;
+				break;
+			}
+			
+			if (flooded) {
+				break;
+			}
+		}
+		if (!passed) {
+			StartCoroutine(FailInstructions());
+			yield break;
+		}
+
+		yield return StartCoroutine(tText.Hide ());
+		yield return StartCoroutine(ShowWaitTap (tText, "destroying hexes earns points", "match the color palette to earn bonus points!"));
+		GameObject.Find ("GUICamera/MiddleRight").GetComponentInChildren<TutorialArrow> ().FadeIn ();
+		yield return StartCoroutine(ShowWaitTap (tText, "as you earn points, this bar fills", "(tap to continue)"));
+		GameObject.Find ("GUICamera/MiddleRight").GetComponentInChildren<TutorialArrow> ().FadeOut ();
+		GameObject.Find ("GUICamera/MoveProgress/tutorialArrow1").GetComponent<TutorialArrow> ().FadeIn ();
+		yield return StartCoroutine(ShowWaitTap (tText, "each turn uses a grey hex", "(tap to continue)"));
+		yield return StartCoroutine(ShowWaitTap (tText, "fill the bar before you use them all!", "(tap to continue)"));
+		GameObject.Find ("GUICamera/MoveProgress/tutorialArrow1").GetComponent<TutorialArrow> ().FadeOut ();
+
+		for (int i = 0; i < 54; i++) {
+			gridPlaces[i].hexaCube.SlowSpawn(Constants.RandomColor()); 
+		}
+
+		for (int i = 0; i < 6; i++) {
+			moves.DoMove();
+		}
+
+		//GameObject.Find ("GUICamera/MoveProgress/tutorialArrow2").GetComponent<TutorialArrow> ().FadeIn ();
+
+		yield return StartCoroutine(tText.Show ("try making a move yourself", "you have one turn left"));
+		disabled = false;
+		flooded = false;
+		destroyed = false;
+		colorSelector.EnableSwap ();
+		targetScore = 8000;
+		yield return StartCoroutine (WaitForMove ());
+		//GameObject.Find ("GUICamera/MoveProgress/tutorialArrow2").GetComponent<TutorialArrow> ().FadeOut ();
+		while (GridBusy())
+			yield return new WaitForSeconds(0.1f);
+		StartCoroutine (NextLevel ());
+		while (GridBusy())
+			yield return new WaitForSeconds(0.1f);
+		yield return StartCoroutine(tText.Hide ());
+		disabled = true;
+		colorSelector.DisableSwap ();
+
+		yield return StartCoroutine(ShowWaitTap (tText, "you ran out of turns", "before the score bar was filled"));
+		yield return StartCoroutine(ShowWaitTap (tText, "when the board is empty...", "it's gameover!"));
+		yield return StartCoroutine(ShowWaitTap (tText, "now you're ready to play!", "tap to start the game"));
+		FadeCam.Instance.FadeOut(() => {Application.LoadLevel("game");});
+	}
+
+	IEnumerator FailInstructions (){
+		TutorialText tText = GameObject.Find ("TutorialText").GetComponentInChildren<TutorialText> ();
+		yield return StartCoroutine(tText.Hide ());
+		yield return StartCoroutine(ShowWaitTap (tText, "not going to follow intructions?", ""));
+		yield return StartCoroutine(ShowWaitTap (tText, "maybe you don't need a tutorial...", ""));
+		yield return StartCoroutine(ShowWaitTap (tText, "...so good luck!", "tap to start the game"));
+		FadeCam.Instance.FadeOut(() => {Application.LoadLevel("game");});
+	}
+
+	IEnumerator ShowWaitTap (TutorialText tText, string s1, string s2) {
+		yield return StartCoroutine(tText.Show (s1, s2));
+		yield return StartCoroutine (WaitForTap ());
+		yield return StartCoroutine(tText.Hide ());
+	}
+
+	IEnumerator WaitForTap () {
+		while (true) {
+			if (InputHandler.Instance.inputSignalDown) break;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator WaitForSwap () {
+		while (true) {
+			if (colorSelector.Current() == Constants.ChooseColor(0)) break;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator WaitForCenterSelect () {
+		PlayerActions pa = GameObject.Find ("PlayerActions").GetComponent<PlayerActions> ();
+		while (true) {
+			if (pa.selected == origin) break;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator WaitForMoveCenter () {
+		disabled = true;
+		PlayerActions pa = GameObject.Find ("PlayerActions").GetComponent<PlayerActions> ();
+		while (pa.selected == origin) {
+			disabled = false;
+			if (flooded || destroyed)
+				break;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator WaitForMove () {
+		while (!flooded && !destroyed) {
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
 }
---
+++ Assets/TutorialArrow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialArrow : MonoBehaviour {
+
+	SpriteRenderer sr;
+
+	void Awake () {
+		sr = GetComponentInChildren<SpriteRenderer> ();
+	}
+
+	public void FadeIn () {
+		StartCoroutine (IFadeIn ());
+	}
+
+	IEnumerator IFadeIn () {
+		float t = 0;
+		while (t < 1) {
+			t = Mathf.Min(1, t + Time.deltaTime * 4);
+			sr.color = new Color(1,1,1,t);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public void FadeOut () {
+		StartCoroutine (IFadeOut ());
+	}
+	
+	IEnumerator IFadeOut () {
+		float t = 1;
+		while (t > 0) {
+			t = Mathf.Min(1, t - Time.deltaTime * 4);
+			sr.color = new Color(1,1,1,t);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}

8b5f53cb155347a16b438be3a2264b7e3997db56
Broxxar <danjohnmoran@gmail.com>
2014-04-03 00:42:52 -0400

    Credits and menu changes
    Added a credits screen and changed some menu navigation buttons to say
    "back" instead of "menu".

---
+++ Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : InteractiveObject {
+	
+	public override void DownAction () {
+		if (FadeCam.Instance.FadeOut(() => {Application.LoadLevel("credits");}))
+			animation.Play ("buttonpress");
+	}
+}

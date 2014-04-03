6fc54b8569136d35f534a3256182e8b0711af76a
Broxxar <danjohnmoran@gmail.com>
2014-04-02 22:23:58 -0400

    Added tutorial button on menu

---
+++ Assets/Scripts/StartTutorialButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartTutorialButton :  InteractiveObject {
+	
+	public override void DownAction () {
+		if (FadeCam.Instance.FadeOut(() => {Application.LoadLevel("tutorial");}))
+			animation.Play ("buttonpress");
+	}
+}
